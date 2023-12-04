29e9e77a7b9d6c9bc5adacbebc23240dbdb1e447
codeFumaça <fakecrazzyy@hotmail.com>
2023-12-04 04:19:23 -0300

    Refactor guild command to add create guild functionality

--- commands/utility/guild.js
+++ commands/utility/guild.js
@@ -1,21 +1,61 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder, PermissionsBitField, ChannelType, CategoryChannel } = require('discord.js');
 const userSchema = require('../../Schemas/user.js');
 
 module.exports = {
-    cooldown: 5,
-    data: new SlashCommandBuilder()
-        .setName('guild')
-        .setDescription('Ações de guild.')
-        .addStringOption(option =>
-			option.setName('category')
-				.setDescription('The gif category')
+	cooldown: 5,
+	data: new SlashCommandBuilder()
+		.setName('guild')
+		.setDescription('Ações de guild.')
+		.addStringOption(option =>
+			option.setName('opcao')
+				.setDescription('Categoria')
 				.setRequired(true)
 				.addChoices(
-					{ name: 'Adicionar', value: 'guild_add' },
-					{ name: 'Remover', value: 'guild_remove' },
-					{ name: 'verificar', value: 'guild_verify' },
-				)),
+					{ name: 'criar', value: 'create' },
+					{ name: 'adicionar', value: 'guild_add' },
+					{ name: 'remover', value: 'guild_remove' },
+				))
+		.addStringOption(option =>
+			option.setName('guild')
+				.setDescription('Guild')
+		)
+		.addUserOption(option =>
+			option.setName('usuario')
+				.setDescription('Usuario')),
 
-    async execute(interaction) {
-    },
+	async execute(interaction) {
+		const { guild } = interaction;
+		const opcao = interaction.options.getString('opcao');
+		const guildParam = interaction.options.getString('guild');
+		const usuario = interaction.options.getMember('usuario');
+
+		if (opcao === 'create') {
+			if (!interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
+				return interaction.reply({ content: 'Você não tem permissão para usar este comando.', ephemeral: true });
+			}
+
+			await guild.roles.create({
+				name: `${guildParam}`,
+				reason: 'Nova guild criada.',
+			}).then(() => {
+				interaction.reply({ content: `Guild criada com sucesso!`, ephemeral: true });
+			})
+
+			let role = await guild.roles.cache.find(role => role.name === `${guildParam}`);
+			await usuario.roles.add(role);
+			await guild.channels.create({
+				name: `${guildParam}`,
+				type: ChannelType.GuildCategory,
+				permissionOverwrites: [
+					{ id: guild.id, deny: [PermissionsBitField.Flags.ViewChannel] },
+					{ id: role, allow: [PermissionsBitField.Flags.ViewChannel] },
+				]
+			}).then(CategoryChannel => {
+				guild.channels.create({ name: `Canal de Texto`, type: ChannelType.GuildText, parent: CategoryChannel }).then(channel => {
+					channel.send(`Bem vindo ao canal de texto da guild ${guildParam}!`);
+				});
+				guild.channels.create({ name: `Canal de Voz`, type: ChannelType.GuildVoice, parent: CategoryChannel });
+			});
+		}
+	}
 };
