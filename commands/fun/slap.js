d20abcbec5f312edc8a10dd38d46068b45441b0a
codeFumaça <fakecrazzyy@hotmail.com>
2023-12-04 02:40:18 -0300

    Add more slap gifs to the slaps array

--- commands/fun/slap.js
+++ commands/fun/slap.js
@@ -9,7 +9,10 @@ const slaps = [
     "https://media1.tenor.com/images/0720ffb69ab479d3a00f2d4ac7e0510c/tenor.gif",
     "https://media1.tenor.com/images/8b80166ce48c9c198951361715a90696/tenor.gif",
     "https://media1.tenor.com/images/6aa432caad8e3272d21a68ead3629853/tenor.gif",
-    "https://media1.tenor.com/images/4ec47d7b87a9ce093642fc8a3c2969e7/tenor.gif"
+    "https://media1.tenor.com/images/4ec47d7b87a9ce093642fc8a3c2969e7/tenor.gif",
+    "https://media.tenor.com/wKKCNA6Ni-MAAAAC/cheh-t1-t1lose.gif",
+    "https://media.tenor.com/MrhME3n9Z2UAAAAC/dungeong.gif",
+    "https://media.tenor.com/MCEtXDF30sgAAAAC/batman-slap-in-the-face.gif"
 ];
 module.exports = {
     data: new SlashCommandBuilder()
